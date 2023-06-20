f8b94a85ef2671ef9b3ad13f7cb65114016e9ed2
blundanji <102884366+blundanji@users.noreply.github.com>
2023-06-19 23:20:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-import pandas
-
+import pandas as pd
+import requests
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -12,28 +12,20 @@ streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered:', fruit_choice)
 
-
-import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-
-fruityvye_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.text(fruityvice_response)
-# write your own comment -what does the next line do? 
-# write your own comment - what does this do?
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice.lower())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+streamlit.text(fruityvice_response.text)
 streamlit.dataframe(fruityvice_normalized)
 
 
